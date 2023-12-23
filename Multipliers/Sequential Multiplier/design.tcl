7c1d15a746687a609f20ba132a629123f49ad716
AhmedOsama198 <ahmed.osama1982002@gmail.com>
2023-12-23 20:51:05 +0200

    feat: added verilog multiplier

--- Multipliers/Sequential Multiplier/design.tcl
+++ Multipliers/Sequential Multiplier/design.tcl
@@ -1,43 +1,12 @@
-# RegN 
-read_file -format verilog  ./source/regN.v
-analyze  -format verilog {./source/regN.v}
-
-
-current_design regN
-link
-elaborate regN
-check_design
-source ./source/reg.sdc	
-compile -exact_map > ./results/compile.log
-write -format verilog -output ./results/Synth_regN.v
 
 # SeqMult 
 read_file -format verilog  ./source/SeqMult.v
 analyze  -format verilog {./source/SeqMult.v}
-
-
-current_design SeqMult
-link
 elaborate SeqMult
+link
 check_design
 source ./source/Mult.sdc	
 compile -exact_map > ./results/compile.log
-write -format verilog -output ./results/Synth_SeqMult.v
-
-
-# SeqMultMain
-
-read_file -format verilog  ./source/SeqMultMain.v
-analyze  -format verilog {./source/SeqMultMain.v}
-
-current_design A_SeqMult
-link
-elaborate A_SeqMult
-check_design
-source ./source/MultMain.sdc	
-compile -exact_map > ./results/compile.log
-write -format verilog -output ./results/Synth_SeqMultMain.v
-
 # # Generate reports 
 report_area > ./results/synth_area.rpt
 report_cell > ./results/synth_cells.rpt
@@ -48,3 +17,5 @@ report_timing  -max_paths 100 > ./results/synth_timing.rpt
 set sdf_file_path "./results/seqMult.sdf"
 report_timing   -max_paths 100   -delay_type  max  -from [all_inputs] -to [all_outputs] > $sdf_file_path
 report_power > ./results/total_power.rpt
+write -format verilog -output ./results/Synth_SeqMult.v
+
