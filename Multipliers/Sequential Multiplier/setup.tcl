887f71eabdd0a884233ae5fe8f892846d49aff52
omarMah <omarcoding@gmail.com>
2023-12-24 21:13:15 +0200

    Broke up with saeed welcome nand

---
+++ Multipliers/Sequential Multiplier/setup.tcl
@@ -0,0 +1,10 @@
+set_app_var search_path "/home/standard_cell_libraries/NangateOpenCellLibrary_PDKv1_3_v2010_12/lib/Front_End/Liberty/NLDM \
+			 /home/mohamed/Desktop/mips "
+
+set_app_var link_library "* NangateOpenCellLibrary_ss0p95vn40c.db"
+set_app_var target_library "NangateOpenCellLibrary_ss0p95vn40c.db"
+set symbol_library  "saed90nm.sdb"
+
+alias h history
+alias rc "report_constraint -all_violators"
+
